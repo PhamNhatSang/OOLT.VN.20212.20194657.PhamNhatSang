25d745ee21a0b888129213cecf740276da8428dc
PhamNhatSang20194657 <102531456+PhamNhatSang20194657@users.noreply.github.com>
2022-05-30 19:35:13 +0700

    Add files via upload

---
+++ Lab07/AimsProject/src/hust/soict/hedspi/aims/Aims.java
@@ -0,0 +1,61 @@
+package hust.soict.hedspi.aims;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import hust.soict.hedspi.aims.media.Book;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.order.Order;
+
+
+public class Aims {
+
+	
+	public static void main(String[] args) {
+	showMenu();
+
+}
+	public static void showMenu() {
+		Order order= new Order();
+		
+		while(true) {  
+		System.out.println("Order Management Application: ");
+		System.out.println("--------------------------------");
+		System.out.println("1. Create new order");
+		System.out.println("2. Add item to the order");
+		System.out.println("3. Delete item by id");
+		System.out.println("4. Display the items list of order");
+		System.out.println("0. Exit");
+		System.out.println("--------------------------------");
+		System.out.println("Please choose a number: 0-1-2-3-4");
+		Scanner keyboard = new Scanner(System.in);
+		int choice = keyboard.nextInt();
+		if(choice==1) {
+			order= new Order();
+		}
+        if(choice==2) {
+			order.Creatitem();
+		}
+if(choice==3) {
+	System.out.print("Nhap id can xoa: ");
+	int id = keyboard.nextInt();
+	order.removeMedia(id);
+	
+		}
+if(choice==4) {
+	order.Printlist();
+	
+}
+if(choice==0) {
+	System.out.println("exit success");
+	break;
+	
+}
+		}
+}
+    public static Order CreatnewOrder() {
+    	Order order=new Order();
+    	return order;
+    }
+   
+	
+}
---
+++ Lab07/AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
@@ -0,0 +1,69 @@
+package hust.soict.hedspi.aims.media;
+import java.util.*; 
+
+public class CompactDisc extends Disc implements Playable {
+private String artist;
+private  ArrayList<Track> tracks = new ArrayList<Track>();
+public CompactDisc() {
+	super();
+}
+public CompactDisc(String title,String category,float cost,int length,String director,String artist,ArrayList<Track> tracks) {
+	super(title,category,cost,length,director);
+	this.artist=artist;
+	this.tracks=tracks;
+}
+public String getArtist() {
+	return artist;
+}
+public boolean CheckTrack(Track tracks) {
+	for(int i=0;i<this.tracks.size();i++) {
+		if(this.tracks.get(i).equals(tracks)==true) {
+			return true;
+		}
+	}
+	return false;
+}
+public void addTrack(Track tracks) {
+	for(int i=0;i<this.tracks.size();i++) {
+		if(CheckTrack(tracks)==true) {
+			System.out.println("The track had been exsited");
+			return;
+		}
+	}
+	this.tracks.add(tracks);
+	System.out.println("Add success");
+}
+public void removeTrack(Track tracks) {
+	for(int i=0;i<this.tracks.size();i++) {
+		if(CheckTrack(tracks)==true) {
+			this.tracks.remove(i);
+			System.out.println("The track is deleted");
+			return;
+			
+		}
+	}
+	System.out.println("Can't find the item");
+}
+public int getLength() {
+	int sumLength=0;
+	for(int i=0;i<this.tracks.size();i++) {
+		sumLength+=this.tracks.get(i).getLength();
+	}
+	return sumLength;
+}
+public void play() {
+	System.out.println("Some imformation of CD: Artist: "+this.getArtist()+"-"+"Director: "+super.getDirector()+"-"+"Length: "+this.getLength());
+    for(int i=0;i<this.tracks.size();i++) {
+    	this.tracks.get(i).play();
+    	System.out.println("**********************");
+    }
+}
+public void Print() {
+	 System.out.println("["+"CD]"+"["+super.getDirector()+"]-["+"["+super.getCategory()+"]-["+super.getCost()+"]-["+super.getTitle()+"]-["+this.getArtist()+"]-["+this.getLength()+"]");
+	 for(int i=0;i<tracks.size();i++) {
+		 System.out.println("Track" +"i"+" title: "+tracks.get(i).getTitle());
+		 System.out.println("Track" +"i"+" length: "+tracks.get(i).getLength());
+
+	 }
+}
+}
